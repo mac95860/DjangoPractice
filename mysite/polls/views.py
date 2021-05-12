464b93cae900cfa1ee9ac6f33ac010ab12d25b86
Michael Cervantes <mac95860@hotmail.com>
2021-05-12 17:04:25 -0500

    OOP in Django

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
 
 # Create your views here.
 def index(request):
     return HttpResponse("HEllo World.You're at the polls index")
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
+
+def index(request):
+    # the - next to pub date, is going to return the list in reverse order 
+    # so that the most recent post is on top
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+    
